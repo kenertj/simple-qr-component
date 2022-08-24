07074d0124f3b2c3aa22779324b1fe750c9cc0da
kenertj <tsoojkk@gmail.com>
2022-08-24 19:58:47 +0300

    almost done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Stack, Typography } from '@mui/material';
+
+import QrPicture from './assets/image-qr-code.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React QR
-        </a>
-      </header>
-    </div>
+
+
+    <Stack
+      sx={{
+        width: 200,
+        height: 300,
+        p: 2,
+
+        backgroundColor: '#fff'
+
+      }}
+    >
+      <img src={QrPicture} />
+      <Typography fontSize="15px" fontWeight="700" color="#3A1212" textAlign="center" mb="2px" >Improve your front-end skills by building projects</Typography>
+      <Typography fontSize="11px" fontWeight="400" color="#3A1212" textAlign="center" >Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</Typography>
+    </Stack>
+
+
   );
 }
 
